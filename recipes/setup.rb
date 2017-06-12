fc6081e5194869184dc794de5f88387b20ff9a52
Alfonso Mantilla <AMS@GMAIL.COM>
2017-06-12 18:09:41 +0200

    Añadimos version 0.2.0

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,7 +1,18 @@
-package 'tree' do
-	action :install
-end
+package "tree"
 
 file "/etc/motd" do
-  content "Property of Alfonso"
+  content "Property of Alfonso
+  	IPADDRESS: #{node['ipaddress']}
+	HOSTNAME: #{node['hostname']}
+	MEMORY: #{node['memory']['total']}
+	CPU: #{node['cpu']['0']['mhz']}
+"
+mode 0644
+owner 'root'
+group 'root'
+
+
 end
+ 
+
+
